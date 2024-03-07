dc9b99b78af3e777dac0d1b7c49397f2ea136e17
harshitvats11 <harshisharma2002@gmail.com>
2024-03-07 22:15:01 +0530

    myfirstcommit

---
+++ myFirstApp/src/components/Card/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="w-20 h-20 flex items-center justify-center bg-red-400 rounded-full p-2 ">
+      <div>Helloo</div>
+    </div>
+  );
+};
+
+export { Card };
